8df99490bfdcf77008b74eabd15afd322ca68e60
tigrebiz <129750404+tigrebiz@users.noreply.github.com>
2025-03-05 16:34:07 +0900

    Docker環境でのNext.jsホットリロード最適化

--- frontend/next.config.mjs
+++ frontend/next.config.mjs
@@ -2,6 +2,15 @@
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
+  
+  // Dockerでのホットリロードを最適化
+  webpackDevMiddleware: config => {
+    config.watchOptions = {
+      poll: 1000,
+      aggregateTimeout: 300,
+    }
+    return config
+  },
 };
 
 export default nextConfig; 
